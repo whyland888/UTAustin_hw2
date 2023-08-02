8b228765a2df6caaaa008f37b8380bdc269c7ad5
whyland888 <88807846+whyland888@users.noreply.github.com>
2023-08-02 12:24:51 -0500

    passing mean value (test)

--- homework/acc_logging.py
+++ homework/acc_logging.py
@@ -18,16 +18,16 @@ def test_logging(train_logger, valid_logger):
         torch.manual_seed(epoch)
         for iteration in range(20):
             dummy_train_loss = 0.9**(epoch+iteration/20.)
-            dummy_train_accuracy = epoch/10. + torch.randn(5)
+            dummy_train_accuracy = epoch/10. + torch.randn(10)
             #raise NotImplementedError('Log the training loss')
             train_logger.add_scalar('loss', dummy_train_loss, iteration)
         #raise NotImplementedError('Log the training accuracy')
-        train_logger.add_scalar('accuracy', dummy_train_accuracy, epoch)
+        train_logger.add_scalar('accuracy', dummy_train_accuracy.mean(), epoch)
         torch.manual_seed(epoch)
         for iteration in range(10):
-            dummy_validation_accuracy = epoch / 10. + torch.randn(5)
+            dummy_validation_accuracy = epoch / 10. + torch.randn(10)
         #raise NotImplementedError('Log the validation accuracy')
-        valid_logger.add_scalar('accuracy', dummy_validation_accuracy, epoch)
+        valid_logger.add_scalar('accuracy', dummy_validation_accuracy.mean(), epoch)
 
 
 if __name__ == "__main__":
