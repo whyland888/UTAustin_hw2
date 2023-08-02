6136b1b582ce7339ceeff1a2651a44d7e400e333
whyland888 <88807846+whyland888@users.noreply.github.com>
2023-08-02 09:28:09 -0500

    implemented logging with tensorboard

--- homework/acc_logging.py
+++ homework/acc_logging.py
@@ -19,12 +19,15 @@ def test_logging(train_logger, valid_logger):
         for iteration in range(20):
             dummy_train_loss = 0.9**(epoch+iteration/20.)
             dummy_train_accuracy = epoch/10. + torch.randn(10)
-            raise NotImplementedError('Log the training loss')
-        raise NotImplementedError('Log the training accuracy')
+            #raise NotImplementedError('Log the training loss')
+            train_logger.add_scalar('train/loss', dummy_train_loss, iteration)
+        #raise NotImplementedError('Log the training accuracy')
+        train_logger.add_scalar('train/accuracy', dummy_train_accuracy, epoch)
         torch.manual_seed(epoch)
         for iteration in range(10):
             dummy_validation_accuracy = epoch / 10. + torch.randn(10)
-        raise NotImplementedError('Log the validation accuracy')
+        #raise NotImplementedError('Log the validation accuracy')
+        valid_logger.add_scalar('valid/accuracy', dummy_validation_accuracy, epoch)
 
 
 if __name__ == "__main__":
