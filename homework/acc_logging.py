721ec79ced12c4524803f2da5bd68c530b690eb9
whyland888 <88807846+whyland888@users.noreply.github.com>
2023-08-03 15:26:26 -0500

    Update acc_logging.py

--- homework/acc_logging.py
+++ homework/acc_logging.py
@@ -15,8 +15,8 @@ def test_logging(train_logger, valid_logger):
     """
 
     # This is a strongly simplified training loop
-    global_step = 0
     for epoch in range(10):
+        global_step = 0
         torch.manual_seed(epoch)
         running_train_accuracy = []
         running_valid_accuracy = []
@@ -27,7 +27,7 @@ def test_logging(train_logger, valid_logger):
             train_logger.add_scalar('loss', dummy_train_loss, 20*epoch + iteration)
             global_step += 1
         train_logger.add_scalar('accuracy', torch.tensor(running_train_accuracy).mean().item(), global_step)
-        global_step = 0
+        
         torch.manual_seed(epoch)
         for iteration in range(10):
             dummy_validation_accuracy = (epoch / 10. + torch.randn(10)).mean().item()
