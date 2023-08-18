1d27a8190a5390056147b6d661239bd0c7dd06ad
whyland888 <88807846+whyland888@users.noreply.github.com>
2023-08-18 08:43:43 -0500

    Update train.py

--- homework/train.py
+++ homework/train.py
@@ -76,7 +76,7 @@ def train(args):
 
 if __name__ == '__main__':
 
-    device = torch_directml.device()  # torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") #torch_directml.device()  
     print(device)
 
     parser = argparse.ArgumentParser()
